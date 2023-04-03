376d9e1995982a3729d9ac2bc633f50681492581
Lunis Ahdidouch <lunisahdidouch@Luniss-MacBook-Pro.local>
2023-04-03 17:21:36 +0200

    Project start

---
+++ RecipeAI/RecipeAI/RecipeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAIApp.swift
+//  RecipeAI
+//
+//  Created by Lunis Ahdidouch on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
