66ccb221291d6cef902458c4ad21f4cce205ddcd
Lunis Ahdidouch <lunisahdidouch@Luniss-MacBook-Pro.local>
2023-04-13 12:32:08 +0200

    contentview leeg

--- RecipeAI/RecipeAI/ContentView.swift
+++ RecipeAI/RecipeAI/ContentView.swift
@@ -1,1 +1,37 @@
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ExplorePage()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Explore")
+                }
+            
+            Text("Chats")
+                .tabItem {
+                    Image(systemName: "message")
+                    Text("Chat")
+                }
+            
+            Text("Groups")
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Groups")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
